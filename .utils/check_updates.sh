3c36ad8aa8de83134bbabcd4adc461233bcf1b1c
Benjamin Stürz <benni@stuerz.xyz>
2022-03-10 14:26:10 +0100

    Fixed check_updates.sh
    Signed-off-by: Benjamin Stürz <benni@stuerz.xyz>

--- .utils/check_updates.sh
+++ .utils/check_updates.sh
@@ -7,7 +7,7 @@ for dir in $(find -maxdepth 1 -type d -not -iname '.*'); do
    both="$(source "$dir/package.build" && echo "$pkgname:$pkgver")"
    pkgname="$(cut -d':' -f1 <<< "$both")"
    pkgver="$(cut -d':' -f2 <<< "$both")"
-   pacman_ver="$(pacman -Qi "$pkgname" 2>/dev/null | sed -n '/^Version/s/^[^.]\+:\s//p')"
+   pacman_ver="$(pacman -Si "$pkgname" 2>/dev/null | sed -n '/^Version/s/^[^.]\+:\s//p')"
    pacman_ver="$(sed 's/^[^:]://;s/-[0-9]\+$//;s/+.*$//' <<< "$pacman_ver")"
 
    [[ $pacman_ver > $pkgver ]] && echo "$pkgname : $pkgver -> $pacman_ver"
